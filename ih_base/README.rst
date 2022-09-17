ee9fdaf98a0f6e65cb5f78b5ca889b52f3a92ba8
chairiman <34017484+chairiman@users.noreply.github.com>
2022-09-17 19:45:58 +0700

    update

---
+++ ih_base/README.rst
@@ -0,0 +1,3 @@
+===========
+Installer Hebat Base
+===========
---
+++ ih_base/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Instalatur Hebat Base",
+    "version": "1.0",
+    "summary": "Instalatur Hebat Base",
+    "author": "Instalatur Hebat",
+    "depends": ["sale_management", "purchase", "project"],
+    "data": [
+
+    ],
+    "installable": True,
+}
