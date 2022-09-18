557b21db3ced63272fe16f7cd5bb12951b214fde
chairiman <34017484+chairiman@users.noreply.github.com>
2022-09-18 15:03:47 +0700

    update

--- ih_base/models/account.py
+++ ih_base/models/account.py
@@ -14,17 +14,17 @@ class AccountMove(models.Model):
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    @api.model
-    def create(self, vals):
-        res = super(AccountMoveLine, self).create(vals)
+    # @api.model
+    # def create(self, vals):
+    #     res = super(AccountMoveLine, self).create(vals)
 
-        analytic_account_id = False
-        if res.move_id.ih_sale_order_id:
-            analytic_account_id = res.move_id.ih_sale_order_id.analytic_account_id
-        elif res.move_id.purchase_id:
-            analytic_account_id = res.move_id.purchase_id.ih_analytic_account_id
+    #     analytic_account_id = False
+    #     if res.move_id.ih_sale_order_id:
+    #         analytic_account_id = res.move_id.ih_sale_order_id.analytic_account_id
+    #     elif res.move_id.purchase_id:
+    #         analytic_account_id = res.move_id.purchase_id.ih_analytic_account_id
 
-        if analytic_account_id:
-            res.analytic_account_id = analytic_account_id
+    #     if analytic_account_id:
+    #         res.analytic_account_id = analytic_account_id
         
-        return res
+    #     return res
