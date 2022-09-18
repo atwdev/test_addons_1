e5e6736c8a5057ed4246f626ebf9d26f67411403
chairiman <34017484+chairiman@users.noreply.github.com>
2022-09-18 23:59:25 +0700

    update

--- ih_base/models/ir_ui_menu.py
+++ ih_base/models/ir_ui_menu.py
@@ -7,16 +7,22 @@ from odoo import models
 class IrUiMenu(models.Model):
     _inherit = 'ir.ui.menu'
 
-    def _load_menus_blacklist(self):
-        res = super()._load_menus_blacklist()
-        # if not self.env.user.has_group('base.group_system'):
-        res.append(self.env.ref('mail.menu_root_discuss').id)
-        res.append(self.env.ref('contacts.menu_contacts').id)
-        res.append(self.env.ref('sale.sale_menu_root').id)
-        res.append(self.env.ref('account.menu_finance').id)
-        res.append(self.env.ref('project.menu_main_pm').id)
-        res.append(self.env.ref('purchase.menu_purchase_root').id)
-        res.append(self.env.ref('base.menu_board_root').id)
-        res.append(self.env.ref('project.menu_project_management').id)
+    # def _load_menus_blacklist(self):
+    #     res = super()._load_menus_blacklist()
+    #     # if not self.env.user.has_group('base.group_system'):
+    #     res.append(self.env.ref('mail.menu_root_discuss').id)
+    #     res.append(self.env.ref('contacts.menu_contacts').id)
+    #     res.append(self.env.ref('sale.sale_menu_root').id)
+    #     res.append(self.env.ref('account.menu_finance').id)
+    #     res.append(self.env.ref('project.menu_main_pm').id)
+    #     res.append(self.env.ref('purchase.menu_purchase_root').id)
+    #     res.append(self.env.ref('base.menu_board_root').id)
         
+    #     return res
+
+    @api.model
+    @api.returns('self')
+    def get_user_roots(self):
+        res = super().get_user_roots()
+        res = self.env.ref('ih_base.menu_root')
         return res
