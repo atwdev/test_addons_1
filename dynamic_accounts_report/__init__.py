0a3fb2d78a08eaff73e4a5dc17ab43729721fd77
chairiman <34017484+chairiman@users.noreply.github.com>
2022-10-03 23:41:45 +0700

    update

--- dynamic_accounts_report/__init__.py
+++ dynamic_accounts_report/__init__.py
@@ -20,7 +20,7 @@
 #
 #############################################################################
 
-from . import controllers
+# from . import controllers
 from . import wizard
 from . import report
 from . import models
