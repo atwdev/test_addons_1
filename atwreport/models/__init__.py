b929ccc74bf257306aed8941d643b8a62b0e539c
atwdev <atwsolardev@gmail.com>
2023-04-01 10:51:30 +0700

    isan [update]

--- atwreport/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in root directory
-##############################################################################
-# flake8: NOQA
-from . import report
